ce609b33bfa0797f26c90f8d98fd7b65fd46d7a6
IDerter <101259146+IDerter@users.noreply.github.com>
2024-06-21 18:55:47 +0300

    UI + new Control
    Add ui elements, update control system, add singletone, models player

--- 3D Runner Test/Assets/Scripts/PlayerMovement.cs
+++ 3D Runner Test/Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,53 @@ namespace ButchersGames
     {
         [SerializeField] private Transform _playerTransform;
         [SerializeField] private float _limitValueX;
+        private bool _isMobile;
+        private bool _swipeDetect = false;
+
+        private void Start()
+        {
+            _isMobile = Application.isMobilePlatform;
+         //   SwipeDetection.OnSwipeInput += OnSwipeInput;
+        }
+
+        private void OnDestroy()
+        {
+        //    SwipeDetection.OnSwipeInput -= OnSwipeInput;
+        }
+
+        //private void OnSwipeInput(float xSwipe)
+        //{
+        //    float halfScreen = Screen.width / 2;
+        //    float xPos = (xSwipe - halfScreen) / halfScreen;
+        //    float finalXPos = Mathf.Clamp(xPos * _limitValueX, -_limitValueX, _limitValueX);
+        //    Debug.Log("")
+        //}
 
         private void Update()
         {
-            if (Input.GetMouseButton(0))
+            
+
+            if (_isMobile)
             {
-                MovePlayer();
+                if (Input.touchCount > 0)
+                {
+                    Touch touch = Input.GetTouch(0);
+                    // Проверяем фазу касания
+                    if (touch.phase == TouchPhase.Moved)
+                    {
+                        float finalXPos = Mathf.Clamp(_playerTransform.localPosition.x + touch.deltaPosition.x * _limitValueX, -_limitValueX, _limitValueX);
+                        // Вычисляем новую целевую позицию на основе касания
+                        _playerTransform.localPosition = Vector3.Lerp(_playerTransform.localPosition, new Vector3(finalXPos,
+                            _playerTransform.localPosition.y), Time.deltaTime * 5);
+                    }
+                }
+            }
+            else
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    MovePlayer();
+                }
             }
         }
 
@@ -24,7 +65,8 @@ namespace ButchersGames
             float xPos = (Input.mousePosition.x - halfScreen) / halfScreen;
             float finalXPos = Mathf.Clamp(xPos * _limitValueX, -_limitValueX, _limitValueX);
 
-            _playerTransform.localPosition = new Vector3(finalXPos, _playerTransform.localPosition.y, 0);   
+            _playerTransform.localPosition = Vector3.Lerp(_playerTransform.localPosition, new Vector3(finalXPos, _playerTransform.localPosition.y), Time.deltaTime * 5);
+            
         }
     }
 }
